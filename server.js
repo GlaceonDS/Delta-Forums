8a315014de4810548f705e1068b728eac5852c92
GlaceonDS <fuecocodrile@gmail.com>
2024-07-12 16:28:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,23 @@ app.use(cors());
 
 let posts = [];
 
+// Fetch all posts
 app.get('/posts', (req, res) => {
     res.json(posts);
 });
 
+// Add a new post
 app.post('/posts', (req, res) => {
     const post = req.body;
     posts.push(post);
     res.status(201).json(post);
+});
+
+// Delete a post
+app.delete('/posts/:title', (req, res) => {
+    const title = req.params.title;
+    posts = posts.filter(post => post.title !== title);
+    res.status(204).send();
 });
 
 app.listen(3000, () => {
